3f7510de540ac88fa25b5bb7f12bc901c1549cad
huyldph <ledinhhuy26032004@gmail.com>
2025-09-23 17:15:59 +0700

    first commit

---
+++ src/test/java/com/example/librarymanagementwebsite/LibraryManagementWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagementwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
