dfc52c0769fbfc35015a98988f3ab1486913ed29
huyldph <ledinhhuy26032004@gmail.com>
2025-09-23 17:19:33 +0700

    configuration for flyway and swagger

---
+++ src/main/java/com/example/librarymanagementwebsite/LibraryManagementWebsiteApplication.java
@@ -0,0 +1,27 @@
+package com.example.librarymanagementwebsite;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementWebsiteApplication {
+
+    public static void main(String[] args) {
+        // Nạp file .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Đẩy các biến vào System properties để Spring Boot có thể đọc được
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("JWT_SIGNER_KEY", dotenv.get("JWT_SIGNER_KEY"));
+
+        SpringApplication.run(LibraryManagementWebsiteApplication.class, args);
+    }
+
+}
