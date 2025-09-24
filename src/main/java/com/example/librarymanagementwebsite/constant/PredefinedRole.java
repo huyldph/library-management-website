01426ab55aa7628cd642f2b5397f73f9b00ba57e
huyldph <ledinhhuy26032004@gmail.com>
2025-09-24 17:40:36 +0700

    feat(account): add PredefinedRole class for role constants

---
+++ src/main/java/com/example/librarymanagementwebsite/constant/PredefinedRole.java
@@ -0,0 +1,8 @@
+package com.example.librarymanagementwebsite.constant;
+
+public class PredefinedRole {
+    public static final String STAFF_ROLE = "STAFF";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+    private PredefinedRole() {}
+}
