a963b1112852af4244056646ea73bed4f598a65e
huyldph <ledinhhuy26032004@gmail.com>
2025-09-29 18:01:47 +0700

    feat(member): implement MemberController and MemberService for member management

---
+++ src/main/java/com/example/librarymanagementwebsite/feature/member/MemberController.java
@@ -0,0 +1,64 @@
+package com.example.librarymanagementwebsite.feature.member;
+
+import com.example.librarymanagementwebsite.feature.member.dto.MemberRequest;
+import com.example.librarymanagementwebsite.feature.member.dto.MemberResponse;
+import com.example.librarymanagementwebsite.util.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class MemberController {
+    MemberService memberService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
+    public ApiResponse<?> getMembers(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+        Page<MemberResponse> members = memberService.getMembers(page, size);
+
+        return ApiResponse.builder()
+                .result(members)
+                .message("Members retrieved successfully")
+                .build();
+    }
+
+    @GetMapping("/{memberId}")
+    public ApiResponse<?> getMemberById(@PathVariable Integer memberId) {
+        MemberResponse memberResponse = memberService.getMemberById(memberId);
+        return ApiResponse.builder()
+                .result(memberResponse)
+                .message("Member retrieved successfully")
+                .build();
+    }
+
+    @PostMapping("/create")
+    public ApiResponse<?> createMember(@Valid @RequestBody MemberRequest memberRequest) {
+        MemberResponse memberResponse = memberService.createMember(memberRequest);
+
+        return ApiResponse.builder()
+                .result(memberResponse)
+                .message("Member created successfully")
+                .build();
+    }
+
+    @PutMapping("/update")
+    public ApiResponse<?> updateMember(
+            @RequestParam("memberId") Integer memberId,
+            @Valid @RequestBody MemberRequest memberRequest) {
+        MemberResponse memberResponse = memberService.updateMember(memberId, memberRequest);
+
+        return ApiResponse.builder()
+                .result(memberResponse)
+                .message("Member updated successfully")
+                .build();
+    }
+}
---
+++ src/main/java/com/example/librarymanagementwebsite/feature/member/MemberService.java
@@ -0,0 +1,61 @@
+package com.example.librarymanagementwebsite.feature.member;
+
+import com.example.librarymanagementwebsite.feature.member.dto.MemberRequest;
+import com.example.librarymanagementwebsite.feature.member.dto.MemberResponse;
+import com.example.librarymanagementwebsite.feature.member.mapper.MemberMapper;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@Slf4j
+public class MemberService {
+    MemberRepository memberRepository;
+    MemberMapper memberMapper;
+
+    public MemberResponse getMemberById(Integer memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found with ID: " + memberId));
+
+        return memberMapper.toResponse(member);
+    }
+
+    public Page<MemberResponse> getMembers(int page, int size) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Member> members = memberRepository.findAll(pageable);
+
+        return members.map(memberMapper::toResponse);
+    }
+
+    public MemberResponse createMember(MemberRequest memberRequest) {
+        Member member = memberMapper.toEntity(memberRequest);
+        memberRepository.save(member);
+
+        log.info("Member created with ID: {}", member.getMemberId());
+
+        return memberMapper.toResponse(member);
+    }
+
+    public MemberResponse updateMember(Integer memberId, MemberRequest memberRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("Member not found with ID: " + memberId));
+
+        memberMapper.updateMember(memberRequest, member);
+        memberRepository.save(member);
+
+        log.info("Member updated with ID: {}", member.getMemberId());
+
+        return memberMapper.toResponse(member);
+    }
+}
